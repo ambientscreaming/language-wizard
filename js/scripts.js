fdd60cb4151ccb3423ede2f5e3b2396f79ea5f3c
Kelsey Burt <ambientscreaming@gmail.com>
2023-11-02 15:54:05 -0700

    branching for drop down menu values

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,38 @@ document.addEventListener("DOMContentLoaded", (e) => {
     const formDemo = document.getElementById("formDemo");
     formDemo.addEventListener("submit", (e) => {
 
-        const colorList = document.getElementById("colorList").value;
+        const roomList = document.getElementById("roomList").value;
+        const foodList = document.getElementById("foodList").value;
 
-        document.getElementById('color').innerText = colorList;
-        document.querySelector("p#colorOutput").removeAttribute("class")
+        const chosenLanguage = languageFromInputs(roomList, foodList);
+
+
+        document.getElementById('langResult').innerText = chosenLanguage;
+        document.querySelector("p#langOutput").removeAttribute("class")
 
         e.preventDefault();
 
     })
+    function languageFromInputs(roomValue, foodValue) {
+        if (roomValue === "jackhammer" && foodValue === "mashed potatoes") {
+            return "ruby";
+        } else if (roomValue === "jackhammer" && foodValue === "chowder") {
+            return "javascript";
+        } else if (roomValue === "jackhammer" && foodValue === "pancake batter") {
+            return "c#";
+        } else if (roomValue === "paintbrush" && foodValue === "mashed potatoes") {
+            return "ruby";
+        } else if (roomValue === "paintbrush" && foodValue === "chowder") {
+            return "javascript";
+        } else if (roomValue === "paintbrush" && foodValue === "pancake batter") {
+            return "c#";
+        } else if (roomValue === "single rose" && foodValue === "mashed potatoes") {
+            return "ruby";
+        } else if (roomValue === "single rose" && foodValue === "chowder") {
+            return "c#";
+        } else {
+            return "javascript";
+        }
 
+    }
 })
